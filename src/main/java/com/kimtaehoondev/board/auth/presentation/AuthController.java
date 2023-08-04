cec23a4366e71eaa9bdcb3871de22e2ea881cf19
kimtaehoonDev <thkimm.dev@gmail.com>
2023-08-04 18:26:08 +0900

    feat(AuthController): POST api/auth/signup 요청시 이메일이 중복된 경우, 400번 에러를 반환

--- src/main/java/com/kimtaehoondev/board/auth/presentation/AuthController.java
+++ src/main/java/com/kimtaehoondev/board/auth/presentation/AuthController.java
@@ -2,6 +2,7 @@ package com.kimtaehoondev.board.auth.presentation;
 
 import com.kimtaehoondev.board.auth.application.AuthService;
 import com.kimtaehoondev.board.auth.presentation.dto.SignUpRequestDto;
+import com.kimtaehoondev.board.exception.EmailDuplicatedException;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +19,19 @@ public class AuthController {
 
     @PostMapping("/signup")
     public ResponseEntity<Long> signUp(SignUpRequestDto dto) {
-        Long savedId = authService.signUp(dto);
+        try {
+            Long savedId = authService.signUp(dto);
 
-        URI location = ServletUriComponentsBuilder.fromCurrentRequestUri()
-            .replacePath("/api/members/" + savedId)
-            .build()
-            .toUri();
+            URI location = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .replacePath("/api/members/" + savedId)
+                .build()
+                .toUri();
 
-        return ResponseEntity.created(location).build();
+            return ResponseEntity.created(location).build();
+        } catch (EmailDuplicatedException e) {
+            return ResponseEntity.badRequest().build();
+        }
+
+
     }
 }
--- src/test/java/com/kimtaehoondev/board/auth/presentation/AuthControllerTest.java
+++ src/test/java/com/kimtaehoondev/board/auth/presentation/AuthControllerTest.java
@@ -2,6 +2,7 @@ package com.kimtaehoondev.board.auth.presentation;
 
 import static org.hamcrest.Matchers.endsWith;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -11,6 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kimtaehoondev.board.auth.application.AuthService;
 import com.kimtaehoondev.board.auth.presentation.dto.SignUpRequestDto;
+import com.kimtaehoondev.board.exception.EmailDuplicatedException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,5 +48,20 @@ class AuthControllerTest {
                 .content(objectMapper.writeValueAsString(dto)))
             .andExpect(status().isCreated())
             .andExpect(header().string("Location", endsWith("/api/members/" + savedId)));
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("회원가입 실패 - 이메일 중복")
+    void duplicatedEmail() throws Exception {
+        SignUpRequestDto dto = new SignUpRequestDto("k@naver.com", "123");
+
+        doThrow(EmailDuplicatedException.class)
+            .when(authService).signUp(any(SignUpRequestDto.class));
+
+        mockMvc.perform(post("/api/auth/signup").with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto)))
+            .andExpect(status().isBadRequest());
     }
 }
