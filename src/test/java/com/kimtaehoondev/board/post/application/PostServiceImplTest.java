7f340632b83b631580662661c52b15d90aeb231b
kimtaehoonDev <thkimm.dev@gmail.com>
2023-08-05 00:25:22 +0900

    test(PostServiceImpl): 게시물 작성 성공/실패 테스트케이스 작성

---
+++ src/test/java/com/kimtaehoondev/board/post/application/PostServiceImplTest.java
@@ -0,0 +1,100 @@
+package com.kimtaehoondev.board.post.application;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.kimtaehoondev.board.exception.MemberNotFoundException;
+import com.kimtaehoondev.board.member.domain.Member;
+import com.kimtaehoondev.board.member.domain.repository.MemberRepository;
+import com.kimtaehoondev.board.post.domain.Post;
+import com.kimtaehoondev.board.post.domain.PostRepository;
+import com.kimtaehoondev.board.post.presentation.PostWriteServiceRequestDto;
+import java.lang.reflect.Field;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class PostServiceImplTest {
+    @InjectMocks
+    private PostServiceImpl postService;
+
+    @Mock
+    private MemberRepository memberRepository;
+    @Mock
+    private PostRepository postRepository;
+
+    @Test
+    @DisplayName("게시물을 작성한다")
+    void writePost() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        String title = "제목";
+        String contents = "내용입니다";
+        Long writerId = 1L;
+        Long postId = 12L;
+        Member writer = makeMember(writerId, "k@naver.com", "123456789");
+        Post post = makePost(postId, title, contents, writer);
+        PostWriteServiceRequestDto dto = new PostWriteServiceRequestDto(title, contents, writerId);
+
+        when(memberRepository.findById(writerId))
+            .thenReturn(Optional.of(writer));
+        when(postRepository.save(any(Post.class)))
+            .thenReturn(post);
+
+        //when
+        Long savedPostId = postService.writePost(dto);
+
+        //then
+        Assertions.assertThat(savedPostId).isEqualTo(post.getId());
+        verify(memberRepository, times(1)).findById(writerId);
+        verify(postRepository, times(1)).save(any(Post.class));
+    }
+
+    @Test
+    @DisplayName("게시물 등록 시 작성자가 존재하지 않으면 예외를 반환한다")
+    void noWriter() {
+        //given
+        String title = "제목";
+        String contents = "내용입니다";
+        Long writerId = 1L;
+        PostWriteServiceRequestDto dto = new PostWriteServiceRequestDto(title, contents, writerId);
+
+        when(memberRepository.findById(dto.getMemberId()))
+            .thenReturn(Optional.empty());
+
+        //when, then
+        Assertions.assertThatThrownBy(() -> postService.writePost(dto))
+            .isInstanceOf(MemberNotFoundException.class);
+        verify(memberRepository, times(1)).findById(writerId);
+        verify(postRepository, never()).save(any(Post.class));
+    }
+
+    private static Post makePost(long id, String title, String content, Member writer)
+        throws NoSuchFieldException, IllegalAccessException {
+        Post post = Post.create("title", "content", writer);
+
+        Field idField = Post.class.getDeclaredField("id");
+        idField.setAccessible(true);
+        idField.set(post, id);
+
+        return post;
+    }
+    private static Member makeMember(long savedId, String email, String pwd)
+        throws NoSuchFieldException, IllegalAccessException {
+        Member member = Member.create(email, pwd);
+
+        Field idField = Member.class.getDeclaredField("id");
+        idField.setAccessible(true);
+        idField.set(member, savedId);
+
+        return member;
+    }
+}
