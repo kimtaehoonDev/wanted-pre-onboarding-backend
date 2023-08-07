4828ffc8ab89c43ff59e7d8a9c07d677e8f96208
kimtaehoonDev <thkimm.dev@gmail.com>
2023-08-07 11:25:32 +0900

    fix(): BaseEntity에 updatedDate가 들어가지 않는 이슈 해결

--- src/main/java/com/kimtaehoondev/board/BaseEntity.java
+++ src/main/java/com/kimtaehoondev/board/BaseEntity.java
@@ -6,7 +6,7 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
@@ -18,6 +18,6 @@ public class BaseEntity {
     private LocalDateTime createdAt;
 
     @Column
-    @LastModifiedBy
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
