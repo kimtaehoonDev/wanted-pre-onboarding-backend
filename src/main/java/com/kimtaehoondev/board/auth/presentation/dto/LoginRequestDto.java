7e08489370c7527ba1d193a496729a2d66cf9aa9
kimtaehoonDev <thkimm.dev@gmail.com>
2023-08-07 16:10:03 +0900

    feat(): 회원가입, 로그인 시 NotNull Validation 추가

--- src/main/java/com/kimtaehoondev/board/auth/presentation/dto/LoginRequestDto.java
+++ src/main/java/com/kimtaehoondev/board/auth/presentation/dto/LoginRequestDto.java
@@ -2,6 +2,7 @@ package com.kimtaehoondev.board.auth.presentation.dto;
 
 import static com.kimtaehoondev.board.auth.presentation.AuthController.EMAIL_CONDITION_REGEX;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -11,8 +12,10 @@ import lombok.Getter;
 @Getter
 public class LoginRequestDto {
     @Pattern(regexp = EMAIL_CONDITION_REGEX, message = "이메일이 형식에 맞지 않습니다.")
+    @NotNull
     private final String email;
 
     @Size(min = 8)
+    @NotNull
     private final String pwd;
 }
--- src/main/java/com/kimtaehoondev/board/auth/presentation/dto/SignUpRequestDto.java
+++ src/main/java/com/kimtaehoondev/board/auth/presentation/dto/SignUpRequestDto.java
@@ -3,6 +3,7 @@ package com.kimtaehoondev.board.auth.presentation.dto;
 import static com.kimtaehoondev.board.auth.presentation.AuthController.EMAIL_CONDITION_REGEX;
 
 import com.kimtaehoondev.board.member.domain.Member;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -12,9 +13,11 @@ import lombok.Getter;
 @Getter
 public class SignUpRequestDto {
     @Pattern(regexp = EMAIL_CONDITION_REGEX, message = "이메일이 형식에 맞지 않습니다.")
+    @NotNull
     private final String email;
 
     @Size(min = 8)
+    @NotNull
     private final String pwd;
 
 }
